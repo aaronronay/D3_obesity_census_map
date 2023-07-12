be649daf907f29aec062af94937e1e40cfa7388b
aaronronay <30646034+aaronronay@users.noreply.github.com>
2023-07-11 22:12:00 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,304 +1,160 @@
-var svgWidth = 900;
-var svgHeight = 500;
+// Define chart configuration
+const chartConfig = {
+  height: 500,
+  width: 800,
+  margin: { top: 20, right: 40, bottom: 80, left: 100 },
+  xAxisLabelX: "Obese (BMI > 30)(%)",
+  xAxisLabelY: "Current Smoker (%)",
+  analysisResponses: [
+    "There is a strong negative correlation (-0.751735757) between having at least a Bachelor's Degree and being obese.",
+    "There is a negative correlation (-0.617179941) between having at least a Bachelor's Degree and being a current smoker.",
+    "There is a positive correlation (0.67396584) between being a high school graduate and being obese.",
+    "There is a strong positive correlation (0.757923374) between being a high school graduate and being a current smoker."
+  ]
+};
 
-var margin = { top: 20, right: 40, bottom: 80, left: 100 };
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3
-  .select(".chart")
+// Select the chart container and append an SVG element
+const svg = d3.select(".chart")
   .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight)
+  .attr("width", 900)
+  .attr("height", 500)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(100, 20)");
 
-// Append an SVG group
-var chart = svg.append("g");
+// Create a tooltip element
+const tooltip = d3.select(".chart").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
 
-// Append a div to the body to create tooltips, assign it a class
-d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
+// Define scales for the x-axis and y-axis
+const yScale = d3.scaleLinear().range([480, 0]);
+const xScale = d3.scaleLinear().range([0, 700]);
 
-// Retrieve data from the CSV file and execute everything below
+// Define axis functions for the x-axis and y-axis
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
+
+// Variables for storing minimum and maximum values
+let xMin, xMax, yMax;
+
+// Function to find minimum and maximum values from data
+function findMinAndMax(dataColumnX) {
+  xMin = d3.min(myData, data => Number(data[dataColumnX]) * 0.8);
+  xMax = d3.max(myData, data => Number(data[dataColumnX]) * 1.1);
+  yMax = d3.max(myData, data => Number(data.bachelorOrHigher) * 1.1);
+}
+
+// Function to write analysis text based on selected axes
+function writeAnalysis(xAxis, yAxis) {
+  const analysisText = document.getElementById('analysis');
+  const responses = chartConfig.analysisResponses;
+  const responseIndex = xAxis === "obese" ? (yAxis === "bachelorOrHigher" ? 0 : 2) : (yAxis === "bachelorOrHigher" ? 1 : 3);
+  analysisText.innerHTML = responses[responseIndex];
+}
+
+// Read data from CSV file
 d3.csv("data.csv", function(err, myData) {
   if (err) throw err;
 
-  myData.forEach(function(data) {
+  // Data preprocessing: convert string values to numbers
+  myData.forEach(data => {
     data.obese = Number(data.obese);
     data.bachelorOrHigher = Number(data.bachelorOrHigher);
     data.currentSmoker = Number(data.currentSmoker);
   });
 
-  console.log(myData);
+  // Create the initial chart
+  createChart("obese", "bachelorOrHigher");
 
-  // Create scale functions
-  var yLinearScale = d3.scaleLinear().range([height, 0]);
+  // Function to create/update the chart based on selected axes
+  function createChart(xAxis, yAxis) {
+    // Calculate the minimum and maximum values for the selected x-axis
+    findMinAndMax(xAxis);
 
-  var xLinearScale = d3.scaleLinear().range([0, width]);
+    // Set the domain for the x-axis and y-axis scales
+    xScale.domain([xMin, xMax]);
+    yScale.domain([0, yMax]);
 
-  // Create axis functions
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
+    // Create a tooltip using D3.tip plugin
+    const toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(data => {
+        const itemName = data.state;
+        const itemEdu = Number(data.bachelorOrHigher);
+        const itemInfo = Number(data[xAxis]);
+        const itemString = xAxis === "obese" ? "Obese: " : "Smoker: ";
+        const eduString = yAxis === "bachelorOrHigher" ? "College Grad: " : "HS Grad: ";
+        return `${itemName}<hr>${eduString}${itemEdu}%<br>${itemString}${itemInfo}%`;
+      });
 
-  // Variables store minimum and maximum values in a column in data.csv
-  var xMin;
-  var xMax;
-  var yMax;
+    // Call the tooltip on the SVG element
+    svg.call(toolTip);
 
-  // Function identifies the minimum and maximum values in a column in data.csv
-  // and assigns them to xMin and xMax variables, which defines the axis domain
-  function findMinAndMax(dataColumnX) {
-    xMin = d3.min(myData, function(data) {
-      return Number(data[dataColumnX]) * 0.8;
-    });
+    // Create circles representing data points
+    const circles = svg.selectAll("circle")
+      .data(myData)
+      .enter()
+      .append("circle")
+      .attr("cx", data => xScale(Number(data[xAxis])))
+      .attr("cy", data => yScale(Number(data.bachelorOrHigher)))
+      .attr("r", "12")
+      .attr("fill", "lightblue")
+      .on("mouseover", toolTip.show)
+      .on("mouseout", toolTip.hide);
 
-    xMax = d3.max(myData, function(data) {
-      return Number(data[dataColumnX]) * 1.1;
-    });
+    // Add state abbreviations as labels for data points
+    const texts = svg.selectAll("text")
+      .data(myData)
+      .enter()
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("class", "stateText")
+      .style("fill", "white")
+      .style("font", "10px sans-serif")
+      .style("font-weight", "bold")
+      .text(data => data.abbr)
+      .on("mouseover", toolTip.show)
+      .on("mouseout", toolTip.hide)
+      .attr("x", data => xScale(Number(data[xAxis])))
+      .attr("y", data => yScale(Number(data.bachelorOrHigher)) + 4);
 
-    yMax = d3.max(myData, function(data) {
-      return Number(data.bachelorOrHigher) * 1.1;
-    });
-  }
+    // Append x-axis to the SVG
+    svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0,480)")
+      .call(xAxis);
 
-  // The default x-axis is 'obese'
-  // Another axis can be assigned to the variable during an onclick event.
-  var currentAxisLabelX = "obese";
+    // Append y-axis to the SVG
+    svg.append("g")
+      .attr("class", "y-axis")
+      .call(yAxis);
 
-  var currentAxisLabelY = "bachelorOrHigher";
+    // Update the axis label for the y-axis
+    svg.select(".axis-text[data-axis-name='bachelorOrHigher']").text("Bachelor's Degree or Greater");
 
-  writeAnalysis(currentAxisLabelX, currentAxisLabelY);
-
-  // Call findMinAndMax() with default
-  findMinAndMax(currentAxisLabelX);
-
-  // Set domain of an axis to extend from min to max values of the data column
-  xLinearScale.domain([xMin, xMax]);
-  yLinearScale.domain([0, yMax]);
-
-  // Initializes tooltip
-  var toolTip = d3
-    .tip()
-    .attr("class", "tooltip")
-    // Define position
-    .offset([80, -60])
-    // The html() method allows mix of JS and HTML in callback function
-    .html(function(data) {
-      var itemName = data.state;
-      var itemEdu = Number(data.bachelorOrHigher);
-      var itemInfo = Number(data[currentAxisLabelX]);
-      var itemString;
-      // Tooltip text depends on which axis is active
-      if (currentAxisLabelX === "obese") {
-        itemString = "Obese: ";
-      }
-      else {
-        itemString = "Smoker: ";
-      }
-      if (currentAxisLabelY === "bachelorOrHigher") {
-        eduString = "College Grad: ";
-      }
-      else {
-        eduString = "HS Grad: ";
-      }
-      return itemName +
-        "<hr>" +
-        eduString +
-        itemEdu + "%<br>" +
-        itemString +
-        itemInfo + "%";
-    });
-
-  // Create tooltip
-  chart.call(toolTip);
-
-  chart
-    .selectAll("circle")
-    .data(myData)
-    .enter()
-    .append("circle")
-    .attr("cx", function(data, index) {
-      return xLinearScale(Number(data[currentAxisLabelX]));
-    })
-    .attr("cy", function(data, index) {
-      return yLinearScale(Number(data.bachelorOrHigher));
-    })
-    .attr("r", "12")
-    .attr("fill", "lightblue")
-    // Both circle and text instances have mouseover & mouseout event handlers
-    .on("mouseover", function(data) {
-      toolTip.show(data)})
-    .on("mouseout", function(data) {
-      toolTip.hide(data)});
-
-  chart
-    .selectAll("text")
-    .data(myData)
-    .enter()
-    .append("text")
-    .attr("text-anchor", "middle")
-    .attr("class","stateText")
-    .style("fill", "white")
-    .style("font", "10px sans-serif")
-    .style("font-weight", "bold")
-    .text(function(data) {
-      return data.abbr;})
-    .on("mouseover", function(data) {
-      toolTip.show(data)})
-    .on("mouseout", function(data) {
-      toolTip.hide(data)})
-    .attr("x", function(data, index) {
-      return xLinearScale(Number(data[currentAxisLabelX]));
-    })
-    .attr("y", function(data, index) {
-      return yLinearScale(Number(data.bachelorOrHigher))+4;
-    });
-
-  // Append an SVG group for the x-axis, then display the x-axis
-  chart
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    // The class name assigned here will be used for transition effects
-    .attr("class", "x-axis")
-    .call(bottomAxis);
-
-  // Append a group for y-axis, then display it
-  chart.append("g")
-    .attr("class", "y-axis")
-    .call(leftAxis);
-
-  // Append y-axis label
-  chart
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - height / 2)
-    .attr("dy", "1em")
-    .attr("class", "axis-text")
-    .attr("data-axis-name", "bachelorOrHigher")
-    .text("Bachelor's Degree or Greater");
-
-  // Append x-axis labels
-  chart
-    .append("text")
-    .attr(
-      "transform",
-      "translate(" + width / 2 + " ," + (height + margin.top + 20) + ")"
-    )
-    // This axis label is active by default
-    .attr("class", "axis-text active")
-    .attr("data-axis-name", "obese")
-    .text("Obese (BMI > 30)(%)");
-
-  chart
-    .append("text")
-    .attr(
-      "transform",
-      "translate(" + width / 2 + " ," + (height + margin.top + 45) + ")"
-    )
-    // This axis label is inactive by default
-    .attr("class", "axis-text inactive")
-    .attr("data-axis-name", "currentSmoker")
-    .text("Current Smoker (%)");
-
-  // Change an axis's status from inactive to active when clicked (if it was inactive)
-  // Change the status of all active axes to inactive otherwise
-  function labelChange(clickedAxis) {
-    d3
-      .selectAll(".axis-text")
-      .filter(".active")
-      // An alternative to .attr("class", <className>) method. Used to toggle classes.
+    // Set the initial active and inactive classes for axis labels
+    svg.selectAll(".axis-text")
       .classed("active", false)
       .classed("inactive", true);
 
-    clickedAxis.classed("inactive", false).classed("active", true);
-    writeAnalysis(currentAxisLabelX, currentAxisLabelY);
+    // Set the active class for the selected x-axis label
+    svg.select(`.axis-text[data-axis-name='${xAxis}']`)
+      .classed("inactive", false)
+      .classed("active", true);
+
+    // Write the analysis text based on the selected axes
+    writeAnalysis(xAxis, yAxis);
   }
 
+  // Event listener for clicking on axis labels
   d3.selectAll(".axis-text").on("click", function() {
-    // Assign a variable to current axis
-    var clickedSelection = d3.select(this);
-    // "true" or "false" based on whether the axis is currently selected
-    var isClickedSelectionInactive = clickedSelection.classed("inactive");
-    // console.log("this axis is inactive", isClickedSelectionInactive)
-    // Grab the data-attribute of the axis and assign it to a variable
-    // e.g. if data-axisName is "poverty," var clickedAxis = "poverty"
-    var clickedAxis = clickedSelection.attr("data-axis-name");
+    const clickedSelection = d3.select(this);
+    const isClickedSelectionInactive = clickedSelection.classed("inactive");
+    const clickedAxis = clickedSelection.attr("data-axis-name");
 
-    // The onclick events below take place only if the x-axis is inactive
-    // Clicking on an already active axis will therefore do nothing
     if (isClickedSelectionInactive) {
-      // Assign the clicked axis to the variable currentAxisLabelX
-      currentAxisLabelX = clickedAxis;
-      // Call findMinAndMax() to define the min and max domain values.
-      findMinAndMax(currentAxisLabelX);
-      // Set the domain for the x-axis
-      xLinearScale.domain([xMin, xMax]);
-      // Create a transition effect for the x-axis
-      svg
-        .select(".x-axis")
-        .transition()
-        // .ease(d3.easeElastic)
-        .duration(1800)
-        .call(bottomAxis);
-
-      // Select all circles to create a transition effect, then relocate its horizontal location
-      // based on the new axis that was selected/clicked
-      d3.selectAll("circle").each(function() {
-        d3
-          .select(this)
-          .transition()
-          // .ease(d3.easeBounce)
-          .attr("cx", function(data, index) {
-            return xLinearScale(Number(data[currentAxisLabelX]));
-          })
-          .duration(1800);
-      });
-
-      d3.selectAll(".stateText").each(function() {
-        d3
-          .select(this)
-          .transition()
-          // .ease(d3.easeBounce)
-          .attr("x", function(data, index) {
-            return xLinearScale(Number(data[currentAxisLabelX]));
-          })
-          .duration(1800);
-      });
-
-      // Change the status of the axes. See above for more info on this function.
-      labelChange(clickedSelection);
+      createChart(clickedAxis, currentAxisLabelY);
     }
   });
 });
-
-function writeAnalysis(xAxis, yAxis) {
-  var analysisText = parent.document.getElementById('analysis');
-
-  var responses = ["There is a strong negative correlation (-0.751735757) between having at least a Bachelor's Degree and being obese.",
-                  "There is a negative correlation (-0.617179941) between having at least a Bachelor's Degree and being a current smoker.",
-                  "There is a positive correlation (0.67396584) between being a high school graduate and being obese.",
-                  "There is a strong positive correlation (0.757923374) between being a high school graduate and being a current smoker."];
-
-  var answer;
-
-  if (xAxis === "obese") {
-    if (yAxis === "bachelorOrHigher") {
-      answer = responses[0];
-    }
-    else {
-      answer = responses[2];
-    }
-  }
-  else {
-    if (yAxis === "bachelorOrHigher") {
-      answer = responses[1];
-    }
-    else {
-      answer = responses[3];
-    }
-  }
-  analysisText.innerHTML = answer;
-};
